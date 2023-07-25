51d52756324f6e93fbfa1cd4af903a1956662677
dv-loginov <dilog@rambler.ru>
2023-07-25 10:46:42 +0300

    feat: Create schemas and API resource models

---
+++ models/movie.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const { Schema } = mongoose;
+
+const movieSchema = new Schema({
+  country: {
+    type: String,
+    required: [true, 'Поле "country" должно быть заполнено'],
+  },
+  director: {
+    type: String,
+    required: [true, 'Поле "director" должно быть заполнено'],
+  },
+  duration: {
+    type: Number,
+    required: [true, 'Поле "duration" должно быть заполнено'],
+  },
+  year: {
+    type: Date,
+    required: [true, 'Поле "year" должно быть заполнено'],
+  },
+  description: {
+    type: String,
+    required: [true, 'Поле "description" должно быть заполнено'],
+  },
+  image: {
+    type: String,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Некорректный URL постера',
+    },
+    required: [true, 'Поле "image" должно быть заполнено'],
+  },
+  trailerLink: {
+    type: String,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Некорректный URL трейлера',
+    },
+    required: [true, 'Поле "trailerLink" должно быть заполнено'],
+  },
+  thumbnail: {
+    type: String,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Некорректный URL миниатюры',
+    },
+    required: [true, 'Поле "thumbnail" должно быть заполнено'],
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+    required: [true, 'Поле "owner" должно быть заполнено'],
+  },
+  movieId: {
+    type: Number,
+    required: [true, 'Поле "movieId" должно быть заполнено'],
+  },
+  nameRU: {
+    type: String,
+    required: [true, 'Поле "nameRU" должно быть заполнено'],
+  },
+  nameEN: {
+    type: String,
+    required: [true, 'Поле "nameRU" должно быть заполнено'],
+  },
+}, { versionKey: false });
+
+module.exports = mongoose.model('movie', movieSchema);
---
+++ models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Поле "name" должно быть заполнено'],
+    minlength: [2, 'Минимальная длина поля "name" - 2'],
+    maxlength: [30, 'Максимальная длина поля "name" - 30'],
+  },
+  email: {
+    type: String,
+    validate: {
+      validator: (v) => validator.isEmail(v),
+      message: 'Некорректный email',
+    },
+    required: [true, 'Поле "email" должно быть заполнено'],
+    unique: true,
+  },
+  password: {
+    type: String,
+    select: false,
+    required: [true, 'Поле "password" должно быть заполнено'],
+  },
+}, { versionKey: false });
+
+module.exports = mongoose.model('user', userSchema);
