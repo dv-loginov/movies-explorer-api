88150852e3a3b2c6b4291a398b2b8415e2e6e63c
dv-loginov <dilog@rambler.ru>
2023-08-02 09:43:12 +0300

    fix: fix errors eslint

--- middlewares/errorsApi.js
+++ middlewares/errorsApi.js
@@ -2,9 +2,11 @@ const { ERROR_MESSAGE } = require('../utils/const');
 
 const errorsApi = (err, req, res, next) => {
   if (err.statusCode) {
-    res.status(err.statusCode).send({ message: err.message });
+    res.status(err.statusCode)
+      .send({ message: err.message });
   } else {
-    res.status(500).send({ message: ERROR_MESSAGE.ERROR_SERVER });
+    res.status(500)
+      .send({ message: ERROR_MESSAGE.ERROR_SERVER });
   }
   next();
 };
